82eb77d0072f1fac1891b536e11e9bb8521b45a9
janidulnkg1 <janidulnkg1@gmail.com>
2023-10-11 11:33:43 +0530

    Add changes

--- ScanDoc/ScanDoc/Program.cs
+++ ScanDoc/ScanDoc/Program.cs
@@ -1,41 +1,84 @@
-﻿using ScanDoc.Logger;
-using System;
+﻿using System;
 using System.IO;
-using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using ScanDoc.Logger;
 
 class Program
 {
-    public required ILogger logging;
+    private static readonly ILogger logging = new Logger();
+    private readonly IConfiguration _configuration;
+
+    public Program(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
     static void Main()
     {
-        string directoryPath = @"C:\DirectoryPath"; 
-        string filePattern = "*.pdf"; 
-
-        // Getting all files in the directory
-        string[] files = Directory.GetFiles(directoryPath, filePattern);
-
-        // Grouping files by their name without extension
-        var fileGroups = files.GroupBy(file =>
-            Path.GetFileNameWithoutExtension(file));
-
-        foreach (var group in fileGroups)
+  
+        try
         {
-            int counter = 0;
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", optional: true, reloadOnChange: true);
 
-            foreach (var oldFileName in group)
+            IConfiguration configuration = builder.Build();
+
+            string sourceDirectory = configuration["SourceDirectory"];
+            string destinationDirectory = configuration["DestinationDirectory"];
+
+
+            // Create the destination directory if it doesn't exist.
+            if (!Directory.Exists(destinationDirectory))
             {
-                counter++;
-                string fileExtension = Path.GetExtension(oldFileName);
-                string newFileName = $"{Path.GetFileNameWithoutExtension(oldFileName)}-{counter:D3}{fileExtension}";
-
-                string newFilePath = Path.Combine(Path.GetDirectoryName(oldFileName), newFileName);
-
-                // Renaming the file according to intended format and moving to intended destination
-                File.Move(oldFileName, newFilePath);
-
-                logging.Log($"Renamed: {oldFileName} to {newFileName}");
+                Directory.CreateDirectory(destinationDirectory);
             }
+
+            // Defining a regular expression pattern to match file format.
+            string pattern = @"^(\d{9}-\d{2})_(.+)\.pdf$";
+
+            string[] pdfFiles = Directory.GetFiles(sourceDirectory, "*.pdf");
+
+            foreach (string filePath in pdfFiles)
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(filePath);
+
+                    // Checking if the file name matches the expected format.
+                    Match match = Regex.Match(fileName, pattern);
+
+                    if (match.Success)
+                    {
+                        string caseNumberCompanyNo = match.Groups[1].Value;
+                        string newFileName = caseNumberCompanyNo + "_" + match.Groups[2].Value + ".pdf";
+                        string destinationFolder = Path.Combine(destinationDirectory, caseNumberCompanyNo);
+
+                        // Create the destination folder if it doesn't exist.
+                        if (!Directory.Exists(destinationFolder))
+                        {
+                            Directory.CreateDirectory(destinationFolder);
+                        }
+
+                        string newFilePath = Path.Combine(destinationFolder, newFileName);
+
+                        // Move the file to the destination folder with the new name.
+                        File.Move(filePath, newFilePath);
+                        logging.Log($"Moved: {filePath} to {newFilePath}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logging.Log($"Error processing file: {filePath}. Error: {ex.Message}");
+                }
+            }
+
+            logging.Log("Processing complete.");
+        }
+        catch (Exception ex)
+        {
+            logging.Log($"Error: {ex.Message}");
         }
     }
 }
