77400c9db64148b046d33f849dea5c55bd40de39
janidulnkg1 <janidulnkg1@gmail.com>
2023-10-11 11:50:30 +0530

    Add changes
    Amended Code to check for files of intended format, but with any extension

--- ScanDoc/ScanDoc/Program.cs
+++ ScanDoc/ScanDoc/Program.cs
@@ -4,81 +4,61 @@ using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using ScanDoc.Logger;
 
-class Program
-{
-    private static readonly ILogger logging = new Logger();
-    private readonly IConfiguration _configuration;
+ILogger logging = new Logger();
 
-    public Program(IConfiguration configuration)
+try
+{
+    IConfigurationBuilder builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+
+    IConfiguration configuration = builder.Build();
+
+    string sourceDirectory = configuration["SourceDirectory"];
+    string destinationDirectory = configuration["DestinationDirectory"];
+
+    if (!Directory.Exists(destinationDirectory))
     {
-        _configuration = configuration;
+        Directory.CreateDirectory(destinationDirectory);
     }
 
-    static void Main()
+    string pattern = @"^(\d{9}-\d{2})_(.+)\..+$"; // Check for files in intended format with any extension
+
+    string[] allFiles = Directory.GetFiles(sourceDirectory); // Getting all available files
+
+    foreach (string filePath in allFiles)
     {
-  
         try
         {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+            string fileName = Path.GetFileName(filePath);
+            Match match = Regex.Match(fileName, pattern);
 
-            IConfiguration configuration = builder.Build();
-
-            string sourceDirectory = configuration["SourceDirectory"];
-            string destinationDirectory = configuration["DestinationDirectory"];
-
-
-            // Create the destination directory if it doesn't exist.
-            if (!Directory.Exists(destinationDirectory))
+            if (match.Success)
             {
-                Directory.CreateDirectory(destinationDirectory);
-            }
+                string caseNumberCompanyNo = match.Groups[1].Value;
+                string newFileName = caseNumberCompanyNo + "_" + match.Groups[2].Value + Path.GetExtension(fileName);
+                string destinationFolder = Path.Combine(destinationDirectory, caseNumberCompanyNo);
 
-            // Defining a regular expression pattern to match file format.
-            string pattern = @"^(\d{9}-\d{2})_(.+)\.pdf$";
-
-            string[] pdfFiles = Directory.GetFiles(sourceDirectory, "*.pdf");
-
-            foreach (string filePath in pdfFiles)
-            {
-                try
+                if (!Directory.Exists(destinationFolder))
                 {
-                    string fileName = Path.GetFileName(filePath);
-
-                    // Checking if the file name matches the expected format.
-                    Match match = Regex.Match(fileName, pattern);
-
-                    if (match.Success)
-                    {
-                        string caseNumberCompanyNo = match.Groups[1].Value;
-                        string newFileName = caseNumberCompanyNo + "_" + match.Groups[2].Value + ".pdf";
-                        string destinationFolder = Path.Combine(destinationDirectory, caseNumberCompanyNo);
-
-                        // Create the destination folder if it doesn't exist.
-                        if (!Directory.Exists(destinationFolder))
-                        {
-                            Directory.CreateDirectory(destinationFolder);
-                        }
-
-                        string newFilePath = Path.Combine(destinationFolder, newFileName);
-
-                        // Move the file to the destination folder with the new name.
-                        File.Move(filePath, newFilePath);
-                        logging.Log($"Moved: {filePath} to {newFilePath}");
-                    }
+                    Directory.CreateDirectory(destinationFolder);
                 }
-                catch (Exception ex)
-                {
-                    logging.Log($"Error processing file: {filePath}. Error: {ex.Message}");
-                }
+
+                string newFilePath = Path.Combine(destinationFolder, newFileName);
+
+                File.Move(filePath, newFilePath);
+                logging.Log($"Moved: {filePath} to {newFilePath}");
             }
-
-            logging.Log("Processing complete.");
         }
         catch (Exception ex)
         {
-            logging.Log($"Error: {ex.Message}");
+            logging.Log($"Error processing file: {filePath}. Error: {ex.Message}");
         }
     }
+
+    logging.Log("Processing complete.");
+}
+catch (Exception ex)
+{
+    logging.Log($"Error: {ex.Message}");
 }
