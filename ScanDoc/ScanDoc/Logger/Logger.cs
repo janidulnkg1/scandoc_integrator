2c87f5f7828df15a74be26655aeb705d66a97dc8
janidulnkg1 <janidulnkg1@gmail.com>
2023-10-11 09:13:54 +0530

    Added Logger package
    NLog

---
+++ ScanDoc/ScanDoc/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanDoc.Logger
+{
+    public class Logger: ILogger
+    {
+        private static readonly NLog.Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public void Log(string message)
+        {
+            _logger.Info(message);
+        }
+    }
+}
